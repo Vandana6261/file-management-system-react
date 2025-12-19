63b0b47612af8efcfd483d8355f4e8a69c22b269
Vandana6261 <vvandanapatidar992@gmail.com>
2025-12-19 14:33:27 +0530

    use this as modal to create file of folder

---
+++ frontend/src/components/CreateFile.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import useFileContext from "./../context/FileContext";
+import useFileManager from "./../hooks/useFileManager";
+
+function CreateFile({open, setOpen}) {
+  const { currentPath } = useFileContext();
+  const { createFileOrFolderApi } = useFileManager();
+  const [fileOrFolderName, setFileOrFolderName] = useState("")
+  const [type, setType] = useState("file")
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log(currentPath, fileOrFolderName, type)
+        if((type == "files" && fileOrFolderName.includes("."))  || type == "folder") {
+            createFileOrFolderApi(currentPath, fileOrFolderName, type);
+            setFileOrFolderName("")
+            setOpen(false);
+        } else {
+            alert("Please give the extension type with file") 
+            return;
+        }
+    }
+
+  return (
+    <>
+      <div className={`modal h-screen w-screen fixed flex justify-center items-center top-0 backdrop-blur-sm text-white z-30 ${open ? "flex" : "hidden"}`} >
+
+        <div className="absolute left-[70%] top-[30%] bg-[#038b7b67] p-2 rounded-full h-8 w-8 flex justify-center items-center cursor-pointer" onClick={() => setOpen(false)}>X</div>
+
+        <div className="modal-content border-2 border-gray-300/30 h-60 w-72 rounded-md p-4">
+
+          <form action="#" className="flex flex-col items-center gap-4">
+            
+            <input type="text" placeholder="Enter file name" name="inputName" value={fileOrFolderName} onChange={(e) => setFileOrFolderName(e.target.value)} required  className="p-2 outline-1 outline-gray-300/30 rounded-md w-full" />
+
+            <div className="btn" className="flex flex-wrap gap-2">
+
+              <div className={`flex-1/3 border-2 border-[#00796B] rounded-md p-2 text-xl transition-all duration-75 ease-linear hover:text-lg hover:scale-95 cursor-pointer ${type === "file" ? "bg-[#00796B]" : ""}`}  onClick={() => setType("file")}>File</div>
+              <div className={`flex-1/3 border-2 border-[#00796B] rounded-md p-2 text-xl transition-all duration-75 ease-linear hover:text-lg hover:scale-95 cursor-pointer ${type === "folder" ? "bg-[#00796B]" : ""}`}  onClick={() => setType("folder")}>Folder</div>
+              <div className="flex-1/3 border-2 border-[#00796B] rounded-md p-2 text-xl transition-all duration-75 ease-linear hover:text-lg hover:scale-95 cursor-pointer" onClick={() => {
+                setOpen(false)
+                setFileOrFolderName("")
+              }}>Cancel</div>
+              <button className="flex-1/3 border-2 border-[#00796B] rounded-md p-2 text-xl transition-all duration-75 ease-linear hover:text-lg hover:scale-95 cursor-pointer" onClick={submitHandler} type="Submit">Create</button>
+
+            </div>
+
+          </form>
+
+        </div>
+
+      </div>
+    </>
+  );
+}
+
+export default CreateFile;
--- frontend/src/hooks/useFileManager.js
+++ frontend/src/hooks/useFileManager.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import useFileContext from "../context/FileContext.jsx"
 
+
 function useFileManager() {
     const {currentPath, setCurrentPath, message, setMessage, filesData, setFilesData, backword, setBackword, forward, setForward} = useFileContext()
 
@@ -17,7 +18,7 @@ function useFileManager() {
         setForward([])
       }
     } catch (error) {
-      console.log(error, "error");
+      console.log("Error in loadFiles :", error);
     }
   }
 
@@ -26,55 +27,105 @@ function useFileManager() {
   }, []);
 
   const getInsideFileApi = async (path) => {
-    console.log("getInsideFile Api called")
-    console.log(path)
-    const response = await fetch("http://localhost:3000", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ filePath: path }),
-    });
-    if (!response.ok) {
-      setMessage("error");
-      return;
-    } else {
-      const data = await response.json();
-      setMessage(data);
-      await getFileApi(path);
+    try {
+      console.log("getInsideFile Api called")
+      console.log(path)
+      const response = await fetch("http://localhost:3000", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ filePath: path }),
+      });
+      if (!response.ok) {
+        setMessage("error");
+        return;
+      } else {
+        const data = await response.json();
+        setMessage(data);
+        await getFileApi(path);
+      }
+    } catch (error) {
+      console.log("Error in getInsideFileApi : ", error)
     }
   };
 
   const getFileApi = async (path) => {
-    console.log("get api called")
-    const response = await fetch("http://localhost:3000");
-    if (!response.ok) {
-      setMessage("error");
-    } else {
-      const data = await response.json();
-      if (Array.isArray(data.body)) {
-        if(backword[backword.length - 1] == path) {
-          setBackword(prev => prev.slice(0, -1))
-        } else if(forward[forward.length - 1] == path) {
-          setForward(prev => prev.slice(0, -1))
+    try {
+      console.log("get api called")
+      const response = await fetch("http://localhost:3000");
+      if (!response.ok)
+      {
+        setMessage("error");
+      } 
+      else
+        {
+        const data = await response.json();
+        if (Array.isArray(data.body)) {
+          if(backword[backword.length - 1] == path) {
+            setBackword(prev => prev.slice(0, -1))
+          } else if(forward[forward.length - 1] == path) {
+            setForward(prev => prev.slice(0, -1))
+          }
+          else {
+            setBackword(prev => [...prev, currentPath])
+          }
+          setCurrentPath(path);
+          setFilesData((prev) => data.body);
+          
         }
         else {
-          setBackword(prev => [...prev, currentPath])
+          setMessage(data.message);
         }
-        setCurrentPath(path);
-        setFilesData((prev) => data.body);
-        
       }
-      else {
-        setMessage(data.message);
-      }
+    }
+    catch(error) 
+    {
+        console.log(error)
     }
   };
 
+  const createFileOrFolderApi = async (path, name, type) => {
+    console.log(path, name, type)
+    try 
+    {
+      const response = await fetch("http://localhost:3000/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          fileOrFolderName: name,
+          type,
+          path
+        })
+      });
+      if(!response.ok) {
+        setMessage("error")
+        console.log("Response is not ok while creating a file or folder");
+      }
+      else 
+      {
+        const data = await response.json();
+        console.log("data received")
+        setMessage(data);
+        console.log("getFileApi calling")
+        await getFileApi(path);
+      }
+    }
+    catch (error) 
+    {
+      console.log("Error in catch block in creatFileOrFolderAPi : ", error)
+    }
+  }
+  
+
   return {
+    loadFiles,
     getInsideFileApi,
     getFileApi,
-    loadFiles
+    createFileOrFolderApi,
+
   }
 }
 
