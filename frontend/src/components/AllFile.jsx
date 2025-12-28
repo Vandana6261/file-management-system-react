e57444166854cf4cba74020af632d5a9d1a7c116
Vandana6261 <vvandanapatidar992@gmail.com>
2025-12-28 23:22:19 +0530

    Bug fix - Double click was not working properly.

--- frontend/src/components/AllFile.jsx
+++ frontend/src/components/AllFile.jsx
@@ -5,9 +5,11 @@ import useFileManager from '../hooks/useFileManager.js';
 function AllFile() {
   const {currentPath, setCurrentPath, filesData, backword, forward, recentFile, setRecentFile} = useFileContext()
   const {getInsideFileApi, getFileApi} = useFileManager()
+  const [selectIndex, setSelectIndex] = useState(-1)
 
-  const getInsideFile = (e) => {
-    const clickedFile = e.target.textContent;
+
+  const getInsideFile = (clickedFile) => {
+    // const clickedFile = e.target.textContent;
     let path = `${currentPath}\\${clickedFile}`
 
     if(clickedFile.includes(".")) {
@@ -25,15 +27,16 @@ function AllFile() {
   
   return (
     <>
-      <div className="text-black dark:text-white w-full p-4 ">
-        <div className="files flex flex-wrap mt-4 gap-4 ">
+      <div className="text-black dark:text-white w-full p-4">
+        <div className="files flex flex-wrap justify-center mt-4 gap-4">
           {/* <div>{backword}</div> */}
           {filesData && filesData.map((item, index) => {
             return (
               <div
                 key={index}
-                className="file bg-card-light dark:bg-card-dark w-30 h-30 rounded cursor-pointer flex flex-col justify-center items-center transition-all duration-200 hover:-translate-y-1.5 hover:scale-105  "
-                onDoubleClick={(e) => getInsideFile(e)}
+                className={`file bg-card-light dark:bg-card-dark w-30 h-30 rounded cursor-pointer flex flex-col justify-center items-center transition-all duration-200 hover:-translate-y-1.5 hover:scale-105 select-none ${selectIndex == index ? "bg-green-900/50 dark:bg-green-300/30" : ""}`}
+                onDoubleClick={() => getInsideFile(item.name)}
+                onClick={() => setSelectIndex(prev => index)}
               >
                 <div className="text-6xl">{item.icon}</div>
                 <p
