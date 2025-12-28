746f46edd9761251fa94e58075896f86618eb97b
Vandana6261 <vvandanapatidar992@gmail.com>
2025-12-28 14:06:16 +0530

    Rewrite the code for recentFile data.

--- frontend/src/components/RecentFile.jsx
+++ frontend/src/components/RecentFile.jsx
@@ -1,47 +1,48 @@
 import React, { useEffect, useState } from 'react'
-import useFileContext from '../context/FileContext'
+import useFileContext, { FileContext } from '../context/FileContext'
 import useFileManager from '../hooks/useFileManager';
 
-function RecentFile({showRecent, setShowRecent}) {
-    const { getRecentFileDataApi } = useFileManager()
-    const [recentArr, setRecentArr] = useState([]);
-    const [data, setData] = useState([])
-    let temp ;
-    useEffect(() => {
-        temp = JSON.parse(localStorage.getItem("recent"));
-        console.log(temp)
-        // recentArr.forEach((eachFilePath) => {
-            
-        // });
-        // getRecentFileDataApi(recentArr[0]);
-        setRecentArr(temp);
-    }, [])
+function RecentFile({ showRecent, setShowRecent }) {
+  const { getRecentFileDataApi } = useFileManager()
+  const { recentFile, setRecentFile } = useFileContext()
+  const [recentArr, setRecentArr] = useState([])
+  const [data, setData] = useState([])
+  let temp;
 
-    const callApi = () => {
-        console.log(recentArr)
-        console.log(recentArr[0])
-        recentArr.forEach(each => {
-          console.log(each)
-          let response
-          getRecentFileDataApi(each).then((response) => setData(prev => [...prev, response]))
-          setData(response)
-        })
+
+  useEffect(() => {
+    let recentTempArr = JSON.parse(localStorage.getItem("recent")) || [];
+    setRecentFile(recentTempArr);
+    setRecentFile([...recentTempArr]);
+    callApi(recentTempArr)
+  }, [])
+
+  const callApi = async (recentTempArr) => {
+    if(recentTempArr.length > 0) {
+      let response = await getRecentFileDataApi(recentTempArr)
+      console.log(response)
+      setData(response)
     }
+  }
   return (
     <>
-      <div className=' bg-amber-700 h-full w-full cursor-pointer' onClick={callApi}>
-            hello
-            {/* {console.log(data)} */}
-            {
-              data && data.length > 0 && data.map((each, index) => (
-                <div>
-                  <p>{data.filePath}</p>
-                  <p>{data.fileData.icon}</p>
-                  <p>{data.fileData.name}</p>
-                  <p>{data.fileData.type}</p>
+      <div className=' bg-amber-700 max-h-[50vh] w-[70vw] rounded-xl p-4 cursor-pointer overflow-auto'>
+        {/* {console.log(data)} */}
+        {
+          data && data.length > 0 && data.map((each, index) => (
+            <div key={index} className=' bg-red-500 p-1 '>
+              <div className='flex justify-between items-center shadow-[0_0_10px_rgba(0,0,0,0.5) border-2 rounded'>
+               <div className='w-fit'>
+                 <p className='text-4xl'>{each.icon}</p>
+               </div>
+                <div className='text-xl p-1 bg-blue-500 w-full '>
+                  <p>{each.name}</p>
+                <p>{each.path}</p>
                 </div>
-              ))
-            }
+              </div>
+            </div>
+          ))
+        }
       </div>
     </>
   )
