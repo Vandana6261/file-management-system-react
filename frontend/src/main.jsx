1878501eab4264e07e03f579e5148e2357468888
Vandana6261 <vvandanapatidar992@gmail.com>
2026-01-11 23:57:51 +0530

    Want to make an component for loading and searching animation.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx' 
 import { FileProvider } from "./context/FileContext";
+import EmptyFile from './components/EmptyFile.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <FileProvider>
-    <App />
+    {/* <App /> */}
+    <EmptyFile />
     </FileProvider>
   </StrictMode>,
 )
