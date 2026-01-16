20bd27565e86730ccbd8b14c4a1ec90afcca66f8
Vandana6261 <vvandanapatidar992@gmail.com>
2026-01-16 12:04:25 +0530

    Few Tweaks.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx' 
 import { FileProvider } from "./context/FileContext";
-import EmptyFile from './components/EmptyFile.jsx';
+import EmptyFolder from './components/EmptyFolder.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <FileProvider>
     <App />
-    {/* <EmptyFile /> */}
+    {/* <EmptyFolder /> */}
     </FileProvider>
   </StrictMode>,
 )
