3866e5ad5fdee74b2e6e3e1c6f51d7263cb6f612
Vandana6261 <vvandanapatidar992@gmail.com>
2026-01-29 23:41:37 +0530

    Animation component.

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <>
+      <div className='text-white flex bg-amber-400 min-h-[82vh] '>
+        <p>Loading....</p>
+        <div className=''>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Loading
